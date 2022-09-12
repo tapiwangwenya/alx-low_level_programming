6513057e044d73f22afb9dc9749b4688b1afe13a
tapiwangwenya <ngwenyatapiwa2@gmail.com>
2022-09-12 08:52:18 -0700

    Programming be hard oo

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+* main - print the string in the put function
+* Description: using the main function
+* this program prints "Programming is travelling the world in one go
+* Return: 0
+*/
+int main(void)
+{
+       puts ("\"Programming is travelling the world in one go");
+       return (0);
+}
