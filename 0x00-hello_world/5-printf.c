7a1d0a344a6022881dcafdea7bbb6cf1b2f9677e
tapiwangwenya <ngwenyatapiwa2@gmail.com>
2022-09-14 02:51:34 -0700

    Coding Eish

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,4 @@
-#imclude <stdio.h>
+#include <stdio.h>
 /**
 * main - print the string in the put function
 * Description: using the main function 
@@ -7,6 +7,6 @@
 */
 int main(void)
 {
-printf("with proper grammar, but outcome is a piece of art");
-return (0);
+	printf("with proper grammar, but outcome is a piece of art");
+	return (0);
 }
