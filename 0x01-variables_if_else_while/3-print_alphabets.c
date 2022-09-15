7d3f5a0419db458337e719290e2f1d59f6ddd195
tapiwangwenya <ngwenyatapiwa2@gmail.com>
2022-09-15 15:18:26 -0700

    Aphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+nclude<stdio.h>
+#include<stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	char w;
+
+	for (w = 'a'; w <= 'z'; w++)
+		putchar(w);
+	for (w = 'A'; w <= 'Z'; w++)
+		putchar(w);
+	putchar('\n');
+	return (0);
+}
