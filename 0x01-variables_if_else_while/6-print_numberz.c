10265a038454b3bec2f94c1e5a1842ba0363de42
tapiwangwenya <ngwenyatapiwa2@gmail.com>
2022-09-16 14:07:47 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main -Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	int m;
+
+	for (m = '0'; m <= '9'; m++)
+		putchar(m);
+	putchar('\n');
+	return (0);
+}
