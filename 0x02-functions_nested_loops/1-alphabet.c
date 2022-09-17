c31a1fcce39c269d0512905a45d6374fa1874171
tapiwangwenya <ngwenyatapiwa2@gmail.com>
2022-09-17 13:16:45 -0700

    I sometimes suffer from insomia

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h
+
+/**
+ * print_alphabet - prints alphabets in lowercase
+ *
+ * author: tuneday
+ * Return: nothing, just void
+ */
+void print_alphabet(void)
+{
+	
+	char t = 'a';
+
+	while (t <= 'z')
+	{
+		_putchar(t);
+		t++;
+	}
+	_putchar('\n');
+}
