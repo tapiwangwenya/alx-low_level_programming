92e4d54cf0e3188d2070e5309ed43777c6f4527d
tapiwangwenya <ngwenyatapiwa2@gmail.com>
2022-09-18 03:01:01 -0700

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabets 10 
+ *
+ * author:tuneday
+ * Return: nothing, just void
+ */
+void print_alphabet_x10(void)
+{
+	int a = 0;
+	char b;
+
+	while (a <= 9)
+	{
+		b = 'a';
+
+		while (b <= 'z')
+		{
+			_putchar(b);
+			b++;
+		}
+		_putchar('\n');
+		a++;
+	}
+}
