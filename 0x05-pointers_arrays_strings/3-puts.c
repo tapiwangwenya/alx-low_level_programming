bb87efe09c906a7f688ba7181c37a330dc8d1db6
tapiwangwenya <ngwenyatapiwa2@gmail.com>
2022-09-19 11:59:04 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include "main.h"
 
-/**
- * _puts - puts a string 
- * @str: string to print 
+/*
+ * _puts - prints a string
+ * @str: string to print
  *
- * Description: prints a string 
+ * Description: prints a string
  * on success: returns no error
  */
 
