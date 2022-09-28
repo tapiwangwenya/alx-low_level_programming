aa39b16d99ab8a7a576d377d6eadfb7dfe7a5126
tapiwangwenya <ngwenyatapiwa2@gmail.com>
2022-09-28 10:35:16 -0700

    2-strlen_recursion.c

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -10,7 +10,7 @@ int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion;
-int is_prime_number(int n)
+int is_prime_number(int n);
 int is_palindrome(char *s);
 void check(char *s, int start, int end, int *flag);
 int wildcmp(char *s1, char *s2);
