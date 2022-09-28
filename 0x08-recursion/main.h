0a485cc28cd3ad8296389c27b71699e064254c67
tapiwangwenya <ngwenyatapiwa2@gmail.com>
2022-09-28 12:58:51 -0700

    all updates

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -10,8 +10,8 @@ int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
-int _sqrt(int n, int x;)
 int is_prime_number(int n);
+int _sqrt(int n, int x);
 int is_prime(int n, int start);
 void check(char *s, int start, int end, int *flag);
 int wildcmp(char *s1, char *s2);
