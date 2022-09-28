a7068b0532f2a2d8989523aea8f8a1d625fc4e71
tapiwangwenya <ngwenyatapiwa2@gmail.com>
2022-09-28 12:07:27 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -10,9 +10,9 @@ int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
-int _sqrt(int n, int x)
+int _sqrt(int n, int x;)
 int is_prime_number(int n);
-int is_palindrome(char *s);
+int is_prime(int n, int start);
 void check(char *s, int start, int end, int *flag);
 int wildcmp(char *s1, char *s2);
 
