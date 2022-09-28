ee52920088885db03e0d0951268248a4134cfca0
tapiwangwenya <ngwenyatapiwa2@gmail.com>
2022-09-28 09:44:23 -0700

    1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,12 +1,11 @@
 #include "main.h"
 
 /**
- * _print_rev_recursion - prints a string reverse
+ * _print_rev_recursion - prints a string in reverse
  * @s: string to be printed
  *
  * Return: void
  */
-
 void _print_rev_recursion(char *s);
 {
 	if (*s != '\0')
