a0f6ea49287a92a4084c7c195e5530a0ae02eb34
tapiwangwenya <ngwenyatapiwa2@gmail.com>
2022-09-28 06:53:24 -0700

    0-memset.c1

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -4,7 +4,7 @@
  * _memset - fills a memory block with a constant byte
  * @s: address to memory block
  * @b: char to be used
- * @n: number of bytes to be used 
+ * @n: number of bytes to be used
  *
  * Rertun: pointer to the memory block
  */
