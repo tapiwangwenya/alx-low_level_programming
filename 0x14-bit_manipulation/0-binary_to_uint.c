1cb58cc979e219c2dfc41dca35fadd6078ad8e18
tapiwangwenya <ngwenyatapiwa2@gmail.com>
2022-10-27 09:05:47 -0700

    taps1

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * binary_to_uint - converts a binary number to an unsigned int
+ * binary_to_uint - converts a binary number to unsigned int
  * @b: string containing the binary number
  *
  * Return: the converted number
