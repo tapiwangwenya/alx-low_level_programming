7f05b49d520c3f489976e45741e44a4a2282304a
tapiwangwenya <ngwenyatapiwa2@gmail.com>
2023-04-10 16:39:30 -0700

    Dynamic Libraries

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/tapiwangwenya/alx-low_level_programming/raw/main/0x18-dynamic_libraries/hacked.soexport
+wget -P /tmp https://github.com/tapiwangwenya/alx-low_level_programming/raw/main/0x18-dynamic_libraries/hacked.so
+export LD_PRELOAD=/tmp/hacked.so
