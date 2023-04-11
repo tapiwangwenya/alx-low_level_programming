a7b461c58eefc3530431ad1a42bc5370684b94ec
tapiwangwenya <ngwenyatapiwa2@gmail.com>
2023-04-10 18:44:41 -0700

    nested loops

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,9 +1,11 @@
 #include "main.h"
+
 /**
  * _isalpha - Check if character is an alphabetic character.
  * @c: type int character
  * Return: 1 if letter, lowercase or uppercase, and 0 otherwise
  */
+
 int _isalpha(int c)
 {
 	if ((c > 64 && c < 91) || (c > 96 && c < 123))
