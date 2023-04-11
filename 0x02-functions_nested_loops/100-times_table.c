8c9082ad4ab17f12179250447502953a1c869901
tapiwangwenya <ngwenyatapiwa2@gmail.com>
2023-04-10 19:06:38 -0700

    Nested

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * print_times_table - prints times table
+ * times_table - prints times table
  * @n : times table to use
  * Return: void
  */
 
-void print_times_table(int n)
+void times_table(int n)
 {
 	int a = 0, rep, b;
 
